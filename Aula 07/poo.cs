98cb3c25c9638b480a0d5c6ad4429be1516ba1b0
GilbertAzevedo <gilbertif1995@outlook.com>
2021-07-10 18:46:51 +0000

    Exemplo de DateTime

---
+++ Aula 07/poo.cs
@@ -0,0 +1,19 @@
+using System;
+
+class MainClass {
+  public static void Main() {
+    Console.WriteLine("Informe uma data yyyy-mm-dd");
+    string s = Console.ReadLine();
+    DateTime x = DateTime.Parse(s);
+    Console.WriteLine(x);
+    x = x.AddDays(1);
+    Console.WriteLine(x.Day);
+    Console.WriteLine($"{x:dd}");
+    Console.WriteLine($"{x:ddd}");
+    Console.WriteLine($"{x:dddd}");
+    Console.WriteLine($"{x:dddd/MMM/yyyy}");
+    x = DateTime.Now;
+    Console.WriteLine(x);
+    
+  }
+}
